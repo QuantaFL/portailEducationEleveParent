3ab246140644508af3790fc959504bb65f271ca0
traorecheikh <atidiane741@gmail.com>
2025-07-11 04:25:55 +0000

    feat: Add ConnectivityListener, AuthInterceptor, and LoginRequest for connectivity monitoring and authentication

---
+++ lib/app/core/interceptors/dio_interceptor.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthInterceptor extends Interceptor {
+  final FlutterSecureStorage secureStorage;
+
+  AuthInterceptor(this.secureStorage);
+
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final token = await secureStorage.read(key: 'auth_token');
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+    return handler.next(options);
+  }
+}
