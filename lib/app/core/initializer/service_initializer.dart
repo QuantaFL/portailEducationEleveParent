50a71b0adc974509624218c562b5dc6d339139cb
traorecheikh <atidiane741@gmail.com>
2025-07-11 13:54:36 +0000

    feat: refactor service initialization to use HiveService for improved structure

--- lib/app/core/initializer/service_initializer.dart
+++ lib/app/core/initializer/service_initializer.dart
@@ -2,14 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:logger/logger.dart';
-import 'package:portail_eleve/app/core/data/models/role.dart';
-import 'package:portail_eleve/app/core/data/models/student.dart';
+import 'package:portail_eleve/app/services/hive_service.dart';
 
 import '../../services/connectivity_controller.dart';
 import '../api/api_client.dart';
-import '../data/models/user.dart';
 
 class ServiceInitializer {
   final Logger logger = Logger();
@@ -41,16 +38,7 @@ class ServiceInitializer {
   /// - Instancie et enregistre le client API avec ses dépendances.
   Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
-
-    await Hive.initFlutter();
-
-    Hive.registerAdapter(RoleAdapter());
-    Hive.registerAdapter(StudentAdapter());
-    Hive.registerAdapter(UserAdapter());
-    await Hive.openBox<User>('users');
-    await Hive.openBox<Student>('students');
-    await Hive.openBox<Role>('roles');
-
+    await HiveService().init();
     await Get.putAsync(() async => ConnectivityController(), permanent: true);
     final ConnectivityController connectivity =
         Get.find<ConnectivityController>();
