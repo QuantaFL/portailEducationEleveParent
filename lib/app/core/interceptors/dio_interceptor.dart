013bdeecdd685ec22b31f9f8fd1f0de0431d5926
traorecheikh <atidiane741@gmail.com>
2025-07-11 13:42:06 +0000

    feat: Add documentation for AuthInterceptor and its request handling

--- lib/app/core/interceptors/dio_interceptor.dart
+++ lib/app/core/interceptors/dio_interceptor.dart
@@ -1,11 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+/// Intercepteur Dio pour ajouter automatiquement le jeton d'authentification
+/// dans les en-têtes des requêtes HTTP sortantes.
 class AuthInterceptor extends Interceptor {
   final FlutterSecureStorage secureStorage;
 
   AuthInterceptor(this.secureStorage);
 
+  /// Intercepte les requêtes HTTP avant l'envoi.
+  ///
+  /// Lit le jeton d'authentification depuis le stockage sécurisé.
+  /// Si le jeton est présent, l'ajoute dans l'en-tête `Authorization` au format `Bearer`.
+  ///
+  /// [options] : les options de la requête.
+  /// [handler] : gestionnaire pour continuer ou arrêter la requête.
   @override
   void onRequest(
     RequestOptions options,
