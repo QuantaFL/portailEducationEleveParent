d94c1ec115b0de4c1f35212a4906a47f21b1a215
traorecheikh <atidiane741@gmail.com>
2025-07-11 04:25:39 +0000

    feat: Implement ApiClient with Dio for network requests and error handling

---
+++ lib/app/core/api/api_client.dart
@@ -0,0 +1,72 @@
+import 'package:dio/dio.dart';
+import 'package:dio_smart_retry/dio_smart_retry.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import '../../services/connectivity_controller.dart';
+import '../interceptors/dio_interceptor.dart';
+
+class ApiClient {
+  final Dio dio;
+  final FlutterSecureStorage secureStorage;
+  final ConnectivityController connectivity;
+
+  ApiClient({
+    required this.dio,
+    required this.secureStorage,
+    required this.connectivity,
+  }) {
+    _setupInterceptors();
+  }
+
+  void _setupInterceptors() {
+    dio.interceptors.clear();
+
+    dio.interceptors.addAll([
+      AuthInterceptor(secureStorage),
+      RetryInterceptor(
+        dio: dio,
+        logPrint: print,
+        retries: 3,
+        retryDelays: const [
+          Duration(seconds: 1),
+          Duration(seconds: 2),
+          Duration(seconds: 4),
+        ],
+        retryEvaluator: (error, _) =>
+            error.type == DioExceptionType.connectionTimeout ||
+            error.type == DioExceptionType.connectionError,
+      ),
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+      ),
+    ]);
+  }
+
+  CancelToken generateCancelToken() => CancelToken();
+
+  Future<Response<T>> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParams,
+    CancelToken? cancelToken,
+  }) {
+    return dio.get<T>(
+      path,
+      queryParameters: queryParams,
+      cancelToken: cancelToken,
+    );
+  }
+
+  Future<Response<T>> post<T>(
+    String path, {
+    dynamic data,
+    CancelToken? cancelToken,
+  }) {
+    return dio.post<T>(path, data: data, cancelToken: cancelToken);
+  }
+}
