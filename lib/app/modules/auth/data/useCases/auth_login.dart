9f5c6ea8ab59af816acd370c2c496d1c51a3602f
traorecheikh <atidiane741@gmail.com>
2025-07-21 03:36:12 +0000

    feat: update AuthLogin to use Hive for student and parent data storage, refactor login flow, and rename bulletin_card widget

--- lib/app/modules/auth/data/useCases/auth_login.dart
+++ lib/app/modules/auth/data/useCases/auth_login.dart
@@ -1,24 +1,53 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:portail_eleve/app/core/api/api_client.dart';
+import 'package:portail_eleve/app/core/data/models/login_response.dart';
+import 'package:portail_eleve/app/core/data/models/parent.dart';
+import 'package:portail_eleve/app/core/data/models/student.dart';
+
+import '../../../../core/data/models/login_request.dart';
 
 class AuthLogin {
   final ApiClient apiClient;
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   AuthLogin({required this.apiClient});
 
-  Future<Map<String, dynamic>> call(String email, String password) async {
+  Box<Student> studentBox = Hive.box<Student>('students');
+  Box<Parent> parentBox = Hive.box<Parent>('parents');
+
+  Future<LoginResponse> call(String email, String password) async {
     if (email.isEmpty || password.isEmpty) {
       throw Exception('Email and password cannot be empty');
     }
     Response response = await apiClient.post(
       "/auth/login",
-      data: {"email": email, "password": password},
+      data: LoginRequest(email: email, password: password).toJson(),
     );
 
     if (response.statusCode == 200) {
-      return response.data;
+      LoginResponse loginResponse = LoginResponse.fromJson(response.data);
+      if (loginResponse.user.roleId == 3) {
+        Response response = await apiClient.get(
+          "/students/${loginResponse.user.id}",
+        );
+        if (response.statusCode != 200) {
+          throw Exception('Failed to fetch student data');
+        } else {
+          Student student = Student.fromJson(response.data);
+          studentBox.put(student.id, student);
+        }
+      } else if (loginResponse.user.roleId == 4) {
+        Response response = await apiClient.get(
+          "/parents/${loginResponse.user.id}",
+        );
+        if (response.statusCode != 200) {
+          throw Exception('Failed to fetch parent data');
+        } else {
+          Parent parent = Parent.fromJson(response.data);
+          parentBox.put(parent.id, parent);
+        }
+      }
+      return loginResponse;
     } else {
       throw Exception(
         'Login failed: ${response.data['message'] ?? 'Unknown error'}',
