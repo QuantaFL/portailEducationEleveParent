0bb171009c2464f6c07eab4e65447c934753689f
traorecheikh <atidiane741@gmail.com>
2025-08-02 21:57:12 +0000

    feat: update ClassModel with new fields and adjust serialization for improved data handling

--- lib/app/core/data/models/class_model.dart
+++ lib/app/core/data/models/class_model.dart
@@ -1,32 +1,39 @@
 import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-part "class_model.g.dart";
+part 'class_model.g.dart';
 
-@HiveType(typeId: 4)
-@JsonSerializable()
+@HiveType(typeId: 15)
+@JsonSerializable(fieldRename: FieldRename.snake)
 class ClassModel {
   @HiveField(0)
-  final int id;
+  final int? id;
+
   @HiveField(1)
-  final String name;
+  final String? name;
+
   @HiveField(2)
-  final String academicYear;
+  final String? level;
+
   @HiveField(3)
-  final String? createdAt;
+  final int? count;
+
   @HiveField(4)
+  final String? createdAt;
+
+  @HiveField(5)
   final String? updatedAt;
 
   ClassModel({
-    required this.id,
-    required this.name,
-    required this.academicYear,
+    this.id,
+    this.name,
+    this.level,
+    this.count,
     this.createdAt,
     this.updatedAt,
   });
 
   factory ClassModel.fromJson(Map<String, dynamic> json) =>
       _$ClassModelFromJson(json);
-
   Map<String, dynamic> toJson() => _$ClassModelToJson(this);
 }
--- lib/app/core/data/models/class_model.g.dart
+++ lib/app/core/data/models/class_model.g.dart
@@ -8,7 +8,7 @@ part of 'class_model.dart';
 
 class ClassModelAdapter extends TypeAdapter<ClassModel> {
   @override
-  final int typeId = 4;
+  final int typeId = 15;
 
   @override
   ClassModel read(BinaryReader reader) {
@@ -17,27 +17,30 @@ class ClassModelAdapter extends TypeAdapter<ClassModel> {
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
     return ClassModel(
-      id: fields[0] as int,
-      name: fields[1] as String,
-      academicYear: fields[2] as String,
-      createdAt: fields[3] as String?,
-      updatedAt: fields[4] as String?,
+      id: fields[0] as int?,
+      name: fields[1] as String?,
+      level: fields[2] as String?,
+      count: fields[3] as int?,
+      createdAt: fields[4] as String?,
+      updatedAt: fields[5] as String?,
     );
   }
 
   @override
   void write(BinaryWriter writer, ClassModel obj) {
     writer
-      ..writeByte(5)
+      ..writeByte(6)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
       ..write(obj.name)
       ..writeByte(2)
-      ..write(obj.academicYear)
+      ..write(obj.level)
       ..writeByte(3)
-      ..write(obj.createdAt)
+      ..write(obj.count)
       ..writeByte(4)
+      ..write(obj.createdAt)
+      ..writeByte(5)
       ..write(obj.updatedAt);
   }
 
@@ -57,18 +60,20 @@ class ClassModelAdapter extends TypeAdapter<ClassModel> {
 // **************************************************************************
 
 ClassModel _$ClassModelFromJson(Map<String, dynamic> json) => ClassModel(
-      id: (json['id'] as num).toInt(),
-      name: json['name'] as String,
-      academicYear: json['academicYear'] as String,
-      createdAt: json['createdAt'] as String?,
-      updatedAt: json['updatedAt'] as String?,
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      level: json['level'] as String?,
+      count: (json['count'] as num?)?.toInt(),
+      createdAt: json['created_at'] as String?,
+      updatedAt: json['updated_at'] as String?,
     );
 
 Map<String, dynamic> _$ClassModelToJson(ClassModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
-      'academicYear': instance.academicYear,
-      'createdAt': instance.createdAt,
-      'updatedAt': instance.updatedAt,
+      'level': instance.level,
+      'count': instance.count,
+      'created_at': instance.createdAt,
+      'updated_at': instance.updatedAt,
     };
