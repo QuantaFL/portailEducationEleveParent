d8b005f2310210d47704e883f1771ee1b6d93518
traorecheikh <atidiane741@gmail.com>
2025-08-02 21:57:07 +0000

    feat: enhance HiveService initialization and update HomeBinding for repository dependencies

--- lib/app/modules/student_home/bindings/home_binding.dart
+++ lib/app/modules/student_home/bindings/home_binding.dart
@@ -1,10 +1,30 @@
 import 'package:get/get.dart';
+import 'package:portail_eleve/app/core/api/api_client.dart';
+import 'package:portail_eleve/app/core/data/repositories/bulletin_repository.dart';
+import 'package:portail_eleve/app/core/data/repositories/student_repository.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
+    if (!Get.isRegistered<BulletinRepository>()) {
+      final apiClient = Get.find<ApiClient>();
+      Get.lazyPut<BulletinRepository>(
+        () => BulletinRepository(apiClient: apiClient),
+      );
+    }
+    if (!Get.isRegistered<StudentRepository>()) {
+      final apiClient = Get.find<ApiClient>();
+      Get.lazyPut<StudentRepository>(
+        () => StudentRepository(apiClient: apiClient),
+      );
+    }
+    Get.lazyPut<HomeController>(
+      () => HomeController(
+        bulletinRepository: Get.find<BulletinRepository>(),
+        studentRepository: Get.find<StudentRepository>(),
+      ),
+    );
   }
 }
--- lib/app/services/hive_service.dart
+++ lib/app/services/hive_service.dart
@@ -1,8 +1,13 @@
 import 'package:hive_flutter/adapters.dart';
+import 'package:portail_eleve/app/core/data/models/academic_year.dart';
+import 'package:portail_eleve/app/core/data/models/class_model.dart';
+import 'package:portail_eleve/app/core/data/models/parent.dart';
 
 import '../core/data/models/role.dart';
 import '../core/data/models/student.dart';
+import '../core/data/models/student_session.dart';
 import '../core/data/models/user.dart';
+import '../core/data/models/user_model.dart';
 
 class HiveService {
   Future<void> init() async {
@@ -11,8 +16,18 @@ class HiveService {
     Hive.registerAdapter(RoleAdapter());
     Hive.registerAdapter(StudentAdapter());
     Hive.registerAdapter(UserAdapter());
+    Hive.registerAdapter(ParentAdapter());
+    Hive.registerAdapter(StudentSessionAdapter());
+    Hive.registerAdapter(UserModelAdapter());
+    Hive.registerAdapter(ClassModelAdapter());
+    Hive.registerAdapter(AcademicYearAdapter());
     await Hive.openBox<User>('users');
     await Hive.openBox<Student>('students');
     await Hive.openBox<Role>('roles');
+    await Hive.openBox<Parent>('parents');
+    await Hive.openBox<StudentSession>('student_sessions');
+    await Hive.openBox<UserModel>('user_models');
+    await Hive.openBox<ClassModel>('class_models');
+    await Hive.openBox<AcademicYear>('academic_years');
   }
 }
